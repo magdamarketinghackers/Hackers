5729ea0215bb4406e817c907b5909d432d017652
Magda <magda@marketing-hackers.com>
2024-05-08 00:54:28 +0100

    changes in index.tsx

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,19 +1,27 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { BuilderComponent, builder } from '@builder.io/react';
 
-const Home = () => {
-  const router = useRouter();
+require('dotenv').config();
 
-  useEffect(() => {
-    // Sprawdzamy, czy użytkownik jest na stronie głównej
-    if (router.pathname === '/') {
-      // Jeśli tak, przekierowujemy go na /saltic
-      router.push('/saltic');
-    }
-  }, [router.pathname]); // Efekt zostanie wykonany za każdym razem, gdy zmieni się ścieżka URL
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-  return null; // Możesz zwrócić null lub inny komponent, strona będzie przekierowana zanim cokolwiek zostanie wyrenderowane
-};
+function Home({ page }) {
+  return (
+    <>
+      {page ? (
+        <BuilderComponent model="page" content={page} />
+      ) : null}
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const page = await builder.get('page', { url: '/' }).promise();
+
+  return {
+    props: { page: page || null },
+    revalidate: 5,
+    fallback: 'blocking'
+  };
+}
 
 export default Home;
