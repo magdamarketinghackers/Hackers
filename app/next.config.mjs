9b1f22ca4eba846df0f7677c2939de1080d388fc
Magda <magda@marketing-hackers.com>
2024-05-07 10:29:44 +0100

    update package.json

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -2,9 +2,11 @@ import BuilderDevTools from "@builder.io/dev-tools/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = BuilderDevTools()(
-  BuilderDevTools()({
-    reactStrictMode: true,
-  })
+  BuilderDevTools()(
+    BuilderDevTools()({
+      reactStrictMode: true,
+    })
+  )
 );
 
 export default nextConfig;
