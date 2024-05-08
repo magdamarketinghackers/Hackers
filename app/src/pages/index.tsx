55e1e3bd7f1faa43707b0e457453dbb216ba6015
Magda <magda@marketing-hackers.com>
2024-05-08 14:26:00 +0100

    new main page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,28 +1,10 @@
-// pages/index.tsx
 import React from "react";
-import { BuilderComponent, builder } from '@builder.io/react';
-import { BuilderContent } from '@builder.io/sdk';
-import { GetStaticProps } from "next";
 
-// Inicjalizacja klucza API Builder.io
-builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
-
-// Funkcja getStaticProps do pobrania zawartości strony głównej
-export const getStaticProps: GetStaticProps = async () => {
-  const homepage = await builder.get('homepage').toPromise();
-
-  return {
-    props: {
-      homepage: homepage || null,
-    },
-  };
-};
-
-// Komponent strony głównej
-export default function HomePage({ homepage }: { homepage: BuilderContent | null }) {
+export default function HomePage() {
   return (
-    <>
-      <BuilderComponent model="homepage" content={homepage} />
-    </>
+    <div style={{ textAlign: "center", margin: "20px" }}>
+      <img src="public/logo.png" alt="Logo Agencji Marketingowej" style={{ width: "150px", marginBottom: "20px" }} />
+      <h1>Agencja Marketingowa Marketing Hackers</h1>
+    </div>
   );
 }
