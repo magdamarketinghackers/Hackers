f9411effb545c0242e7b6bd9efe428e74d901413
Magda <magda@marketing-hackers.com>
2024-05-14 13:35:22 +0100

    new

--- app/src/pages/[...page].tsx
+++ app/src/pages/[...page].tsx
@@ -4,57 +4,32 @@ import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
 import DefaultErrorPage from "next/error";
 import Head from "next/head";
 import { BuilderContent } from "@builder.io/sdk";
-import { GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import "../builder-registry";
 import '@builder.io/widgets';
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-// Define a function that fetches the Builder
-// content for a given page
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  // Fetch the builder content for the given page
+export const getServerSideProps: GetServerSideProps = async ({ params, req }) => {
+  const urlPath = params?.page ? "/" + (params.page as string[]).join("/") : "/";
+  const host = req.headers.host;
+
   const page = await builder
     .get("page", {
       userAttributes: {
-        urlPath: "/" + ((params?.page as string[])?.join("/") || ""),
+        urlPath,
+        domain: host,
       },
     })
     .toPromise();
 
-  // Return the page content as props
   return {
     props: {
       page: page || null,
     },
-    // Revalidate the content every 5 seconds
-    revalidate: 5,
   };
 };
 
-// Define a function that generates the
-// static paths for all pages in Builder
-export async function getStaticPaths() {
-  // Get a list of all pages in Builder
-  const pages = await builder.getAll("page", {
-    // We only need the URL field
-    fields: "data.url",
-    options: { noTargeting: true },
-  });
-
-  // Generate the static paths for all pages in Builder
-  return {
-    paths: pages
-      .map((page) => String(page.data?.url))
-      .filter((url) => url !== "/"),
-    fallback: "blocking",
-  };
-}
-
-
-  
-
-// Define the Page component
 export default function Page({ page }: { page: BuilderContent | null }) {
   const router = useRouter();
   const isPreviewing = useIsPreviewing();
@@ -63,27 +38,19 @@ export default function Page({ page }: { page: BuilderContent | null }) {
   const keywords = page?.data?.keywords || "Default Meta Keywords";
   const image = page?.data?.image;
 
-
-  // If the page content is not available
-  // and not in preview mode, show a 404 error page
   if (!page && !isPreviewing) {
     return <DefaultErrorPage statusCode={404} />;
   }
 
-  // If the page content is available, render
-  // the BuilderComponent with the page content
   return (
     <>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
-
-        {/* Social Sharing Tags */}
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
-        {/* Ładowanie favicony z CMS jeśli dostępna, inaczej domyślna */}
         <link rel="icon" href={page?.data?.favicon} type="image/x-icon" />
       </Head>
       <BuilderComponent model="page" content={page || undefined} />
