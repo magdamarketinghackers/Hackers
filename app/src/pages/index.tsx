77b799afcbaeef6bdea0a4c4e9187f116af228fe
magdamarketinghackers <144922808+magdamarketinghackers@users.noreply.github.com>
2024-05-14 12:00:57 +0100

    Update index.tsx

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -30,18 +30,16 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 
-  const title = page?.data?.title || "Default Meta Title";
-  const description = page?.data?.description || "Default Meta Description";
-  const keywords = page?.data?.keywords || "Default Meta Keywords";
-
-  // Extracting Social Sharing fields from page content
-  const image = page?.data?.image;
+  
 
 // Define the Page component
 export default function Home({ page }: { page: BuilderContent | null }) {
   const router = useRouter();
   const isPreviewing = useIsPreviewing();
-
+  const title = page?.data?.title || "Default Meta Title";
+  const description = page?.data?.description || "Default Meta Description";
+  const keywords = page?.data?.keywords || "Default Meta Keywords";
+  const image = page?.data?.image;
   // If the page content is not available
   // and not in preview mode, show a 404 error page
   if (!page && !isPreviewing) {
