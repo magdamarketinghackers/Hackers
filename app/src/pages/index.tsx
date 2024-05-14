82926b7c554f00c4b2af1c9120e09ba62d26ed3b
Magda <magda@marketing-hackers.com>
2024-05-14 10:22:29 +0100

    check

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BuilderComponent, builder } from '@builder.io/react';
+import { BuilderComponent, builder, useIsPreviewing } from '@builder.io/react';
+import { useRouter } from "next/router";
+import { BuilderContent } from "@builder.io/sdk";
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import DefaultErrorPage from 'next/error';
@@ -24,27 +26,28 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-// Define the type for props
-interface HomePageProps {
-  page: any;
-}
 
-const HomePage: React.FC<HomePageProps> = ({ page }) => {
-  // Display a 404 page if no homepage content is availablee
-  if (!page) {
+// Define the Page component
+export default function Home({ page }: { page: BuilderContent | null }) {
+  const router = useRouter();
+  const isPreviewing = useIsPreviewing();
+
+  // If the page content is not available
+  // and not in preview mode, show a 404 error page
+  if (!page && !isPreviewing) {
     return <DefaultErrorPage statusCode={404} />;
   }
 
+  // If the page content is available, render
+  // the BuilderComponent with the page content
   return (
     <>
       <Head>
-        <title>{page?.data?.title || 'Dynamiczna Strona'}</title>
+        <title>{page?.data?.title}</title>
         {/* Ładowanie favicony z CMS jeśli dostępna, inaczej domyślna */}
-        <link rel="icon" href={page?.data?.favicon } type="image/x-icon" />
+        <link rel="icon" href={page?.data?.favicon} type="image/x-icon" />
       </Head>
       <BuilderComponent model="page" content={page || undefined} />
     </>
   );
-};
-
-export default HomePage;
+}
