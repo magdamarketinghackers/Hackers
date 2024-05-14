c9bf84b576d2a633a532bce9e015d7f638db9486
Magda <magda@marketing-hackers.com>
2024-05-14 10:40:50 +0100

    font

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -11,6 +11,9 @@ import { Montserrat } from "next/font/google";
 // Initialize Builder.io with your public API key
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+
 export const getStaticProps: GetStaticProps = async () => {
   // Fetch the homepage content from Builder.io
   const page = await builder.get('page', {
@@ -46,6 +49,11 @@ export default function Home({ page }: { page: BuilderContent | null }) {
         <title>{page?.data?.title}</title>
         {/* Ładowanie favicony z CMS jeśli dostępna, inaczej domyślna */}
         <link rel="icon" href={page?.data?.favicon} type="image/x-icon" />
+        <style jsx global>{`
+        html {
+          font-family: ${montserrat.style.fontFamily};
+        }
+      `}</style>
       </Head>
       <BuilderComponent model="page" content={page || undefined} />
     </>
