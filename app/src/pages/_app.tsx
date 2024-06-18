5711e0eecb5694bff1dd26590d166e184497f741
magdamarketinghackers <144922808+magdamarketinghackers@users.noreply.github.com>
2024-06-18 11:46:49 +0100

    Update _app.tsx

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,24 +1,8 @@
 import "@/styles/globals.css";
-import { Playfair_Display } from 'next/font/google';
 import type { AppProps } from "next/app";
-
-const playfairDisplay = Playfair_Display({
-  weight: ['400'],
-  style: ['italic'],
-  subsets: ['latin'],
-  display: 'swap',
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <style jsx global>{`
-        html, body {
-          font-family: ${playfairDisplay.style.fontFamily};
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </>
-  );
+      <Component {...pageProps} />;
 }
 
