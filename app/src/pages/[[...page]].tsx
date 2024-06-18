29b538f63ae08ebbfa4e6e2661b3eab516757919
magdamarketinghackers <144922808+magdamarketinghackers@users.noreply.github.com>
2024-06-18 10:36:42 +0100

    Update [[...page]].tsx

--- app/src/pages/[[...page]].tsx
+++ app/src/pages/[[...page]].tsx
@@ -78,6 +78,7 @@ export default function Page({ page }: { page: BuilderContent | null }) {
         <meta property="og:image" content={image} />
         {/* Ładowanie favicony z CMS jeśli dostępna, inaczej domyślna */}
         <link rel="icon" href={page?.data?.favicon} type="image/x-icon" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" />
       </Head>
       <BuilderComponent model="macerat" content={page || undefined} /> 
     </>
