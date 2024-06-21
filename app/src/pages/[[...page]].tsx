9b86cc7f80b94e00579ba78affa3c255b833face
magdamarketinghackers <144922808+magdamarketinghackers@users.noreply.github.com>
2024-06-21 11:00:48 +0100

    Update [[...page]].tsx

--- app/src/pages/[[...page]].tsx
+++ app/src/pages/[[...page]].tsx
@@ -77,9 +77,9 @@ export default function Page({ page }: { page: BuilderContent | null }) {
         <meta property="og:image" content={image} />
         {/* Ładowanie favicony z CMS jeśli dostępna, inaczej domyślna */}
         <link rel="icon" href={page?.data?.favicon} type="image/x-icon" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
       </Head>
       <BuilderComponent model="macerat" content={page || undefined} /> 
     </>
